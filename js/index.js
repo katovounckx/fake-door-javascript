5d5570b6fa1493837d72c549a76d7fa8ba3f2bb8
Kato Vounckx <kato.vounckx@student.howest.be>
2022-10-25 13:38:20 +0200

    vite

---
+++ js/index.js
@@ -0,0 +1,16 @@
+import JSConfetti from "js-confetti";
+import "../css/style.css";
+
+const init = () => {
+    const $form = document.querySelector("form");
+    const jsConfetti = new JSConfetti();
+
+    $form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        jsConfetti.addConfetti();
+    });
+    
+}
+
+init ();
